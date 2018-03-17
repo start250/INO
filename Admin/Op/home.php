781f0cddc2e6d9705e531618f5cc1a21bb05025a
AiMuhire <AIM@myPositivo.kuikdelivery.com>
2018-03-17 15:08:11 +0200

    home page initialized

--- Admin/Op/home.php
+++ Admin/Op/home.php
@@ -7,8 +7,8 @@ $sql = "SELECT * FROM Books WHERE section='".$_SESSION["section"]."'";
     $result = $conn->query($sql);
     while($row = $result->fetch_assoc()) {
         ?>
-
-<div class="card bg-info w-75">
+<br>
+<div class="card w-75">
   <div class="card-body">
     <h5 class="card-title"><?= $row["title"]?></h5>
     <p class="card-text"><?= $row["description"]?></p>
