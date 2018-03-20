b233c4f9f10845d474b29d41926a8a10af31ae86
Anaclet <37176750+anaclet922@users.noreply.github.com>
2018-03-20 20:21:34 +0200

    upload Book with all details

--- Op/upload_files.php
+++ Op/upload_files.php
@@ -8,10 +8,54 @@ echo '<h3>Book Information...</h3>';
 
 <input class="form-control" type="text" placeholder="Title" name="title"><br>
 <input class="form-control" type="text" placeholder="Author" name="author"><br>
+<select class="form-control" name="category">
+    <?php
+    if(@$_GET['section']=="academic")
+        echo "<option>Select Category</option>
+    <option>Science Book</option>
+    <option>Humanity Boook</option>
+    <option>Language Book</option>
+    <option>Life Book</option>
+    <option>ICT Book</option>
+    <option>Business Book</option>";
+    else if(@$_GET['section']=="business")
+        echo "<option>Select Category</option>
+    <option>Business Opportunities</option>
+    <option>Business Advice</option>
+    <option>Business News</option>
+    <option>Biographies</option>";
+    else if(@$_GET['section']=="health")
+        echo "<option>Select Category</option>
+    <option>Nutrion</option>
+    <option>Traditional Medecine</option>
+    <option>Serious Deseases</option>
+    <option>Doctor Advice</option>";
+    else if(@$_GET['section']=="eng_class")
+        echo "<option>Select Category</option>
+    <option>Beginners</option>
+    <option>Intermediate</option>
+    <option>Advanced</option>
+    <option>For Business</option>
+    ";
+    else if (@$_GET['section']=="culture")
+       echo "<option>Select Category</option>
+   <option>Amateka</option>
+    <option>Imigani</option>
+    <option>Ibisakuzo</option>
+    <option>Kirazira</option>
+    <option>Ibisakuzo</option>"; 
+    else if(@$_GET['section']=="rules_road")
+        echo "<option>Select Category</option>
+   <option>Igazeti</option>
+    <option>Ibibazo n'ibisubizo</option>
+    <option>Ibyapa</option>";
+    ?>
+</select><br>
 <textarea placeholder="Description..." name="description" class="form-control"></textarea><br>
 <input class="form-control" type="number" placeholder="pages" name="pages"><br>
 <input class="form-control" type="text" placeholder="Added By..." name="added_by" value="<?php echo $f_ad['username'].$f_ad1['username']; ?>"><br> 
-<input hidden type="text"  id="size" name="size"><br> 
+<input hidden type="text"  id="size" name="size"><br>Thumbnail
+<input type="file" name="thumb" class="form-control"><br>Book
 <input id="myFile"  class="form-control" onchange="myFunction()" type="file" name="fileToUpload">
 
 <p id="filesInfo"></p> 
@@ -64,11 +108,12 @@ $target_path =$_SERVER['DOCUMENT_ROOT']."/INOGIT/Resources/Storage/Books/";
   $fullName=basename( $_FILES["fileToUpload"]["name"]);
   list($partName,$extension)=explode('.', $fullName);
 $book_name=urlencode($partName."-".time().".".$extension);
-  $target_path .= $book_name;   
+  $target_path .= $book_name;  
+ $thumb_target=$_SERVER['DOCUMENT_ROOT']."/INOGIT/Resources/Storage/Thumbs/".basename($_FILES['thumb']['name']);
 if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_path)) {   
     echo "File uploaded successfully!";  
-
-
+if (move_uploaded_file($_FILES["thumb"]["tmp_name"], $thumb_target)) 
+    echo "OK";
     //save info to db
     $title=mysqli_real_escape_string($conn,$_POST['title']);
 $author=mysqli_real_escape_string($conn,$_POST['author']);
@@ -76,13 +121,14 @@ $description=mysqli_real_escape_string($conn,$_POST['description']);
 $pages=mysqli_real_escape_string($conn,$_POST['pages']);
 $size=$_POST['size']; 
 $added_by=mysqli_real_escape_string($conn,$_POST['added_by']);
-$updated_at=mysqli_real_escape_string($conn,$_POST['title']);
-$thumb=mysqli_real_escape_string($conn,$_POST['title']); 
+//$updated_at=mysqli_real_escape_string($conn,$_POST['title']);
+$thumb=mysqli_real_escape_string($conn,basename( $_FILES["thumb"]["name"])); 
 $book_link=mysqli_real_escape_string($conn,$book_name);
 $section=$_SESSION['section']; 
-$sql = "INSERT INTO Books (title, author, description, pages, size, added_by, updated_at,thumb,book_link,section) 
+$book_category=$_POST['category'];
+$sql = "INSERT INTO Books (title, author, description, pages, size, added_by, thumb,book_link,section, book_category) 
   
-                   VALUES ('$title', '$author', '$description', '$pages', '$size', '$added_by', '$updated_at','$thumb','$book_link','$section')";
+                   VALUES ('$title', '$author', '$description', '$pages', '$size', '$added_by', '$thumb','$book_link','$section','$book_category')";
 
 if ($conn->query($sql) === TRUE) {
     echo '<script type="text/javascript">
