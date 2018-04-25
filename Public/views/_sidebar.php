e8dacfe10bf44e22ad9e767d4ed7e02f8fb2269f
AiMuhire <AIM@myPositivo.kuikdelivery.com>
2018-04-25 22:14:39 +0200

    translate integrated
    All pages with sidebar support translate

--- Public/views/_sidebar.php
+++ Public/views/_sidebar.php
@@ -4,7 +4,17 @@
     width:40px;
     
 }
+#google_translate_element{
+  float:right;
+}
+
 </style>
+<div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'en,fr,sw', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, multilanguagePage: true}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+        
  
         <ul>
     <?php if(!@$_GET['s']) {?>        
